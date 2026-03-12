d8bda52eeeb3dabf01ab62e56d6f1adb97150f41
nuribostan <120714200+nuribostan@users.noreply.github.com>
2026-03-12 11:41:34 +0300

    Add konaklamaliTurlar.js with tour data

---
+++ konaklamaliTurlar.js
@@ -0,0 +1,48 @@
+const konaklamali = [
+  {
+    id: 1,
+    title: "Doğu Ekspresi Turu",
+    img: "/assets/mevcutTurlarmiz/samuel-ferrara-1527pjeb6jg-unsplash.webprize.jpg",
+    url: "/konaklamali-turlar/dogu-ekspresi-turu",
+    location: "Rize",
+    duration: "1 Gün",
+  },
+
+  {
+    id: 2,
+    title: "Büyük Gap Turu",
+    img: "/assets/mevcutTurlarmiz/samuel-ferrara-1527pjeb6jg-unsplash.webprize.jpg",
+    url: "/konaklamali-turlar/buyuk-gap-turu",
+    location: "Rize",
+    duration: "1 Gün",
+  },
+
+  {
+    id: 3,
+    title: "Batı Karadeniz Turu",
+    img: "/assets/mevcutTurlarmiz/samuel-ferrara-1527pjeb6jg-unsplash.webprize.jpg",
+    url: "/konaklamali-turlar/batı-karadeniz-turu",
+    location: "Rize",
+    duration: "1 Gün",
+  },
+
+  {
+    id: 4,
+    title: "Kapadokya Turu",
+    img: "/assets/mevcutTurlarmiz/samuel-ferrara-1527pjeb6jg-unsplash.webprize.jpg",
+    url: "/konaklamali-turlar/kapadokya-turu",
+    location: "Rize",
+    duration: "1 Gün",
+  },
+
+  {
+    id: 5,
+    title: "Adana-Hatay-Mersin  Turu",
+    img: "/assets/mevcutTurlarmiz/samuel-ferrara-1527pjeb6jg-unsplash.webprize.jpg",
+    url: "/konaklamali-turlar/adana-hatay-mersin-turu",
+    location: "Rize",
+    duration: "1 Gün",
+  },
+];
+
+export default konaklamali;
